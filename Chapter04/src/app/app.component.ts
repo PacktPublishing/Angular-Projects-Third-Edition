07575b7fb69562771400b76942135d1ff0fba35a
Aristeidis Bampakos <bampakoa@gmail.com>
2023-04-02 19:36:31 +0300

    Filter out version updates other than the VERSION_READY event.

--- Chapter04/src/app/app.component.ts
+++ Chapter04/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { SwUpdate } from '@angular/service-worker';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { filter, map, switchMap } from 'rxjs/operators';
 
 @Component({
@@ -15,9 +15,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.updates.versionUpdates.pipe(
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
       switchMap(() => this.snackbar.open('A new version is available!', 'Update now').afterDismissed()),
       filter(result => result.dismissedByAction),
       map(() => this.updates.activateUpdate().then(() => location.reload()))
     ).subscribe();
-  }   
+  }
 }
